b38f976285f14fa9afeab4b655c51f5851479f15
Ilya Kharin <akscram@gmail.com>
2016-11-21 20:19:16 +0300

    Extend examples by using endpoints

--- examples/service/service.py
+++ examples/service/service.py
@@ -15,11 +15,13 @@
 import json
 import logging
 import os
+from six.moves import urllib
 
 from BaseHTTPServer import BaseHTTPRequestHandler
 from BaseHTTPServer import HTTPServer
 
 import entrypoint
+from entrypoint import catalog
 from entrypoint import config
 
 CONF = config.CONF
@@ -28,15 +30,43 @@ SERVICE_NAME = os.environ.get("SERVICE_NAME", "service")
 DEFAULT_CONF = "/etc/conf.json"
 DEFAULT_PORT = 8000
 
+# Initialize configuration.
 entrypoint.process_entrypoint(SERVICE_NAME, default_config_path=DEFAULT_CONF)
 
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
+        request = urllib.parse.urlsplit(self.path)
+        if request.path.endswith("config"):
+            self.handle_config(request)
+        elif request.path.endswith("endpoints"):
+            self.handle_endpoints(request)
+        else:
+            self.handle_service_name(request)
+
+    def handle_config(self, request):
         self.send_response(200)
         self.send_header("Content-type", "text/json")
         self.end_headers()
         json.dump(CONF, self.wfile)
+
+    def handle_endpoints(self, request):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        params = urllib.parse.parse_qs(request.query)
+        service_name = params["service"][0]
+        # Retrieve endpoint for the sevice with the name which is stored
+        # in the variable service_name
+        endpoint = catalog.get_endpoint(service_name)
+        url = endpoint.url()
+        self.wfile.write(url)
+
+    def handle_service_name(self, request):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(SERVICE_NAME)
 
 
 def main():
