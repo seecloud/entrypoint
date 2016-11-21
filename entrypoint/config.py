b16274b9014d9414c6838f5934e943d7a32491f2
Ilya Kharin <akscram@gmail.com>
2016-11-21 23:52:47 +0300

    Preferable to use absolute imports for py27

--- entrypoint/config.py
+++ entrypoint/config.py
@@ -12,6 +12,8 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from __future__ import absolute_import
+
 import json
 
 from entrypoint.args import VARIABLES
